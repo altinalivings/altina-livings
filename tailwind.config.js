0662f42b13436edbc450199bdf6ffeed00ad5326
altinalivings <altinalivings@gmail.com>
2025-08-30 00:26:59 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1e40af',
+        accent: '#ea580c',
+      },
+    },
   },
   plugins: [],
 }
