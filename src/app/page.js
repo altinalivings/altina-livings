cc9f1390f150c1d58b037a6fd1898cb3e992fb1d
altinalivings <altinalivings@gmail.com>
2025-08-29 16:09:53 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 import { CheckCircle, Clock, DollarSign } from 'lucide-react'
 
 export default function Home() {
